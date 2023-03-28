dbd7def76c9c8bf614bd656bfaa7b5c54b3f895e
Fainsteve <fainstevekassums@gmail.com>
2023-03-28 10:56:02 -0700

    00

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,6 +2,20 @@
 """Define a class Square."""
 
 class Square:
-    """Represent a square."""
-    pass
+    """Represent a square.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        """Initialize a new Square object.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.size ** 2
 
