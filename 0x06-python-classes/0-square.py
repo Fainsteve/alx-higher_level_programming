4f9174e351d937321ab5e60e21c4d225c71d1f4a
Fainsteve <fainstevekassums@gmail.com>
2023-08-11 08:50:25 -0700

    my correction

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a class Square with a private attribute size and public methods."""
+"""defines class square """
+
 
 class Square:
-    """Represents a square."""
-    def __init__(self, size):
-        """Initializes a new square with the given size."""
-        self.__size = size
-
-if __name__ == '__main__':
-    my_square = Square()
-    print((type(my_square), my_square.__dict__))
-
+    """ empty square """
+    pass
 
