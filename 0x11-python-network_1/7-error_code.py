c2279a2c26087162fe171f991ce3576c3eea0910
Fainsteve <fainstevekassums@gmail.com>
2024-04-27 07:14:27 -0700

    0x11_

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""A script that
-- takes in a URL
-- sends a request to the URL
-- displays the body of the response.
+"""Sends a request to a given URL and displays the response body.
+
+Python script that takes in a URL,
+sends a request to the URL and displays the body of the response.
 """
 import sys
 import requests
@@ -11,8 +11,8 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+    m = requests.get(url)
+    if m.status_code >= 400:
+        print("Error code: {}".format(m.status_code))
     else:
-        print(r.text)
+        print(m.text)
